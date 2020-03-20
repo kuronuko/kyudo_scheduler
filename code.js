2111072ceb4b6b233187ec8584da651b40118579
kuronuko <kureha5570@gmail.com>
2020-03-20 15:02:03 +0900

    ファビコンを設定

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
 function doGet() {
-  var template = 'index';
-  return HtmlService.createTemplateFromFile(template).evaluate().setTitle('弓道スケジューラー');
+  var htmlOutput = HtmlService.createTemplateFromFile('index').evaluate()
+  htmlOutput
+    .setTitle('弓道スケジューラー')
+    .setFaviconUrl('https://drive.google.com/uc?id=1gJ7C0L-qlelarZym7u5DLJjmOBkV5Yl0&.png');
+  return htmlOutput;
 }
 
 function myFunction() {
